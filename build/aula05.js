7ef7e191a4d3924f51dad3caafd6ab4ba81b6c74
LuisNunesH <luis.nunes@sptech.school>
2024-06-01 13:04:16 -0300

    aula 5

---
+++ build/aula05.js
@@ -0,0 +1,14 @@
+"use strict";
+// let vteste:string|number|boolean;
+let vteste;
+vteste = 'Luis';
+vteste = 2024;
+vteste = true;
+let cursos = ["Javascript", "Typescript", "C++", 100, false];
+let valores = [10, 200, 2023];
+cursos.push("Arduino");
+cursos.push(1000);
+valores.push(555);
+console.log(cursos);
+console.log(valores);
+console.log(vteste);
---
+++ src/aula05.ts
@@ -0,0 +1,17 @@
+// let vteste:string|number|boolean;
+let vteste:any;
+vteste = 'Luis';
+vteste = 2024;
+vteste = true;
+
+let cursos = ["Javascript", "Typescript", "C++", 100, false];
+let valores = [10, 200, 2023];
+
+cursos.push("Arduino");
+cursos.push(1000);
+valores.push(555);
+
+console.log(cursos);
+console.log(valores);
+console.log(vteste);
+
