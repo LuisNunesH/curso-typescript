065339ec1cd2aba512113d8a619e4388bd4b2746
LuisNunesH <luis.nunes@sptech.school>
2024-06-01 12:05:45 -0300

    início do curso

---
+++ build/aula04.js
@@ -0,0 +1,8 @@
+"use strict";
+let nome;
+nome = 'Luis';
+nome = 'Luis 2';
+let valor;
+valor = 10;
+console.log(valor);
+console.log(nome);
---
+++ src/aula04.ts
@@ -0,0 +1,9 @@
+let nome:string;
+nome = 'Luis';
+nome = 'Luis 2';
+
+let valor:number;
+valor = 10;
+
+console.log(valor);
+console.log(nome);
