abb93d55f0d11e9e868a3025cc6ebecba5ef57b0
LuisNunesH <luis.nunes@sptech.school>
2024-06-03 23:28:25 -0300

    aula 8

---
+++ build/aula08.js
@@ -0,0 +1,17 @@
+"use strict";
+let dados = {
+    nome: "Luis",
+    idade: 21,
+    status: "A",
+    ola: () => {
+        console.log("Teste");
+    },
+    info: (p) => {
+        console.log(p);
+    }
+};
+dados.nome = "Luis Henrique";
+console.log(typeof (dados));
+console.log(dados.nome);
+dados.ola();
+dados.info(dados.nome);
