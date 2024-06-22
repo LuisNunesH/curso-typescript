4d6d0eafc9a8631935f232d5f5f886fbbb417783
LuisNunesH <luis.nunes@sptech.school>
2024-06-21 23:27:19 -0300

    aula 35

---
+++ build/aula35.js
@@ -0,0 +1,9 @@
+"use strict";
+// let [aa, bb, cc, dd] = ["Verde", "Vermelho", "Azul", "Amarelo"];
+const obj = {
+    cor1: "Verde",
+    cor2: "Vermelho",
+    cor3: "Azul",
+    cor4: "Amarelo"
+};
+let { cor1, cor2, cor3, cor4 } = obj;
